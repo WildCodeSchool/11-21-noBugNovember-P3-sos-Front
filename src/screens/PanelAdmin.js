629194d11ca23623fc1dac9357e858c5898931fe
Thymus <thymus@pop-os.localdomain>
2022-02-22 15:26:05 +0100

    Création du formulaire d'ajout d'article

--- src/screens/PanelAdmin.js
+++ src/screens/PanelAdmin.js
@@ -1,8 +1,12 @@
 import './Styles/PanelAdmin.css'
+import ArticleForm from "../components/ArticleForm";
 const PanelAdmin =()=>{
     return (
-        <div className='panelAdmin-Container'>
+        <div className='panel-admin-container'>
             <div className='side-bar'>test</div>
+            <div className='content-container'>
+                <ArticleForm />
+            </div>
         </div>
     )
 }
