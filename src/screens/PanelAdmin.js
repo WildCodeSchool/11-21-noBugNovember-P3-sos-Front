23be01a93d58d0deb17deb634fabdc90c26fb585
Thymus <thymus@pop-os.localdomain>
2022-02-22 16:25:02 +0100

    Modification des dispositions d'éléments

--- src/screens/PanelAdmin.js
+++ src/screens/PanelAdmin.js
@@ -4,12 +4,15 @@ const PanelAdmin =()=>{
     return (
         <div className='panel-admin-container'>
             <div className='side-bar'>
+              <img className='logo-panel' src='' alt="logo panel"/>
               <div className='bloc-btn-side-bar'>
+                <input className='search-bar' placeholder='Rechercher'/>
                 <button className='btn-side-bar'>Catégories</button>
                 <button className='btn-side-bar'>Sous-Catégories</button>
                 <button className='btn-side-bar'>Villes</button>
                 <button className='btn-side-bar'>Articles</button>
               </div>
+              <div><button className='btn-validation'>Valider</button> </div>
             </div>
             <div className='content-container'>
                 <ArticleForm />
