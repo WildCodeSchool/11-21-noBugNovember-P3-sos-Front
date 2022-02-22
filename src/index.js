9ed45645e81bf879704793129352d5dd3211cffe
Thymus <thymus@pop-os.localdomain>
2022-02-22 17:35:14 +0100

    Supression des buttons dans la side bar pour ajouter des Links et import de react router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/screens/PanelAdmin.js
+++ src/screens/PanelAdmin.js
@@ -1,18 +1,18 @@
 import './Styles/PanelAdmin.css'
+import { Link } from "react-router-dom";
 import ArticleForm from "../components/ArticleForm";
 const PanelAdmin =()=>{
     return (
         <div className='panel-admin-container'>
             <div className='side-bar'>
               <img className='logo-panel' src='' alt="logo panel"/>
-              <div className='bloc-btn-side-bar'>
-                <input className='search-bar' placeholder='Rechercher'/>
-                <button className='btn-side-bar'>Catégories</button>
-                <button className='btn-side-bar'>Sous-Catégories</button>
-                <button className='btn-side-bar'>Villes</button>
-                <button className='btn-side-bar'>Articles</button>
+              <div className='bloc-link-side-bar'>
+                <Link to="/ArticleForm">Articles</Link>
+                <Link to="">Catégories</Link>
+                <Link to="">Sous-Catégories</Link>
+                <Link to="">Villes</Link>
               </div>
-              <div><button className='btn-validation'>Valider</button> </div>
+
             </div>
             <div className='content-container'>
                 <ArticleForm />
