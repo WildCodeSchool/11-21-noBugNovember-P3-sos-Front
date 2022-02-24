26bdd75f41e8ee31bfbf704e0e3aaf6d8cffb101
Thymus <thymus@pop-os.localdomain>
2022-02-24 15:27:23 +0100

    Création du formulaire d'ajout d'article

--- src/components/ArticleForm.js
+++ src/components/ArticleForm.js
@@ -4,20 +4,58 @@ const ArticleForm =()=>{
     const [article,setArticle]=useState({title:"",urlImg:"",textArticle:""})
     return(
       <div className='article-form-container'>
-          <form className='article-form'>
-            <label>
-              Titre:
-              <input className='input-article-title' placeholder="Titre de l'article" value={article.title}/>
-            </label>
-            <label>
-              Url Image Bannière:
-              <input placeholder="Url de l'image" value={article.urlImg}/>
-            </label>
-            <label>
-              Article:
-              <textarea value={article.textArticle}/>
-            </label>
-          </form>
+        <h2 className='bjr-user'>Bonjour Rachid,</h2>
+
+          <div className="articles-and-types">
+            
+            <div className="bloc-article">
+              <h3 className="titre-colonne">Nouvel article</h3>
+              
+              <form className='article-form'>
+                
+                  <input className='input-article-title' placeholder="Titre de l'article" value={article.title} />
+                             
+                  <input className='input-article-intro' placeholder="Intro de l'article" value={article.intro} />
+                                                      
+                  <input placeholder="Url de l'image" value={article.urlImg}/>
+                                   
+                  <textarea placeholder="Article" value={article.textArticle}/>
+                  <input placeholder='URL des liens à télécharger' value={article.urlTelechargement}/>
+                  
+              </form>
+            </div>
+
+            <div className="types-articles">
+            <h3 className="titre-colonne">Type d'articles</h3>
+
+                <form className="drop-down-type">
+                        <select name="type-opt" className="list-deroulante">
+                          <option>Type 1</option>
+                          <option>Type 2</option>
+                          <option>Type 3</option>
+                        </select>
+                        <select name="secteur-opt" className="list-deroulante">
+                          <option>Secteur 1</option>
+                          <option>Secteur 2</option>
+                          <option>Secteur 3</option>
+                        </select>
+                        <select name="categorie-opt" className="list-deroulante">
+                          <option>Catégorie 1</option>
+                          <option>Catégorie 2</option>
+                          <option>Catégorie 3</option>
+                        </select>
+                        <select name="scategorie-opt" className="list-deroulante">
+                          <option>Sous-Catégorie 1</option>
+                          <option>Sous-Catégorie 2</option>
+                          <option>Sous-Catégorie 3</option>
+                        </select>
+                </form>
+            </div>
+
+
+
+            </div>
+          
       </div>
     )
 }
