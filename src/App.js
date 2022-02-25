ae1a1e7d5c825e85922946c4ae3f6ff8e08b134f
Pimpus <milhan.verschelle@gmail.com>
2022-02-25 10:20:23 +0100

    commit pour clara

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
-import PanelAdmin from "./screens/PanelAdmin";
 import Home from "./screens/Home";
+import PanelAdmin from "./screens/PanelAdmin";
 import FirstVisit from "./screens/FirstVisit";
 
 function App() {
   return (
     <div className="App">
-      <FirstVisit />
-      {/* <Home /> */}
+      {/* <FirstVisit /> */}
+      <Home />
       {/* <PanelAdmin /> */}
     </div>
   );
