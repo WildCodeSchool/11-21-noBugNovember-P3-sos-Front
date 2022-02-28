f6f85f29282e78a13393939f1b5bfb72b0a37858
Pimpus <milhan.verschelle@gmail.com>
2022-02-28 15:01:49 +0100

    creation components SearchBar

--- src/screens/ArticlesGrid.js
+++ src/screens/ArticlesGrid.js
@@ -1,13 +1,15 @@
-import Header from '../components/Header.js'
-import CardArticle from '../components/CardArticle.js'
+import Header from "../components/Header.js";
+import CardArticle from "../components/CardArticle.js";
+import SearchBar from "../components/SearchBar.js";
 
 function Articles() {
   return (
-    <div className='bgGreen'>
+    <div className="bgGreen">
       <Header />
+      <SearchBar />
       <CardArticle />
     </div>
-  )
+  );
 }
 
-export default Articles
+export default Articles;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,56 +1,31 @@
-import './Styles/Home.css'
+import SearchBar from "../components/SearchBar";
+import "./Styles/Home.css";
 
 const Home = () => {
   return (
-    <div className='wrapperHome'>
-      <div className='logoHolder'>
+    <div className="wrapperHome">
+      <div className="logoHolder">
         <img
-          src={require('../assets/logo.png')}
-          alt='logo Sos jeunes pousses'
+          src={require("../assets/logo.png")}
+          alt="logo Sos jeunes pousses"
         />
       </div>
-      <div className='wrapRaw'>
+      <div className="wrapRaw">
         <h1>
           Les mêmes chances pour tous <br />
           Découvrez ...
         </h1>
-        <div className='splitWrap'>
-          <div className='splitOrange'>
-            <div className='holderBtn'>
+        <div className="splitWrap">
+          <div className="splitOrange">
+            <div className="holderBtn">
               <h2>Première Visite ?</h2>
               <button> Commencer</button>
             </div>
-            <div className='holderSearchBar'>
-              <h2>Tu connais la plate-forme ?</h2>
-              <div className='SearchBar'>
-                <input
-                  type='text'
-                  name='searchBar'
-                  placeholder='Rechercher'
-                ></input>
-
-                <select name='citySelect' id='citySelect'>
-                  <option value='' disabled selected hidden>
-                    Ville
-                  </option>
-                  <option value='Grenoble'>Grenoble</option>
-                  <option value='Paris'>Paris</option>
-                </select>
-                <select name='catSelect' id='catSelect'>
-                  <option value='' disabled selected hidden>
-                    Catégories
-                  </option>
-                  <option value='Grenoble'>Jeunesse</option>
-                  <option value='Pré-incubation'>Pré-incubation</option>
-                  <option value='Ideation'>Ideation</option>
-                  <option value='Amorçage'>Amorçage</option>
-                  <option value='Outils de gestions'>Outils de gestions</option>
-                </select>
-              </div>
-              <button> Chercher</button>
-            </div>
+            <h2>Tu connais la plate-forme ?</h2>
+            <SearchBar />
+            <button className="searchButton"> Chercher</button>
           </div>
-          <div className='splitGreen'>
+          <div className="splitGreen">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -69,7 +44,7 @@ const Home = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
