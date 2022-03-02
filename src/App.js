aa471eacfed78e37a8dc2cf8528a1d73512afbf8
Pimpus <milhan.verschelle@gmail.com>
2022-03-02 15:21:31 +0100

    commit modif avec clara

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css'
-import Home from './screens/Home'
-import PanelAdmin from './screens/PanelAdmin'
-import FirstVisit from './screens/FirstVisit'
-import ArticlesGrid from './screens/ArticlesGrid'
+import "./App.css";
+import Home from "./screens/Home";
+import PanelAdmin from "./screens/PanelAdmin";
+import FirstVisit from "./screens/FirstVisit";
+import ArticlesGrid from "./screens/ArticlesGrid";
 
 function App() {
   return (
-    <div className='App'>
-      {/* <FirstVisit /> */}
+    <div className="App">
+      <FirstVisit />
       {/* <Home /> */}
       {/* <PanelAdmin /> */}
-
-      <ArticlesGrid />
+      {/* <ArticlesGrid /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
