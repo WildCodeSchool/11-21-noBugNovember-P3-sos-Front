1ea565ecec410892dd15db824e057b989de2a5b7
Martin Clara <clara-martin_student2021@wilder.school>
2022-03-21 18:10:38 +0100

    end of the day, update articledetails css

--- src/screens/ArticleDetail.js
+++ src/screens/ArticleDetail.js
@@ -1,77 +1,74 @@
-import { ArticleContext } from "../context/ArticleContext";
-import Header from "../components/Header";
-import { useContext } from "react";
-import { useParams, Link } from "react-router-dom";
-import "./Styles/ArticleDetail.css";
-import fleche from "../assets/icones/flecheCercle.png";
+import { ArticleContext } from '../context/ArticleContext'
+import Header from '../components/Header'
+import { useContext } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import './Styles/ArticleDetail.css'
+import fleche from '../assets/icones/flecheCercle.png'
 
 const ArticleDetail = () => {
-  const { articles } = useContext(ArticleContext);
-  console.log(articles);
-  let { id } = useParams();
+  const { articles } = useContext(ArticleContext)
+  console.log(articles)
+  let { id } = useParams()
   return (
     <>
       <Header />
-      <div className="detailArticlegGreen">
-        <div className="stateCatArticlesDet">
-          <Link to="/articlesGrid">
-            <button className="buttonGreen btn-green-form2">Retour</button>
+      <div className='detailArticlegGreen'>
+        <div className='stateCatArticlesDet'>
+          <Link to='/articlesGrid'>
+            <button className='buttonGreen btn-green-form2'>Retour</button>
           </Link>
         </div>
-        <div className="articleDetailWrapper">
+        <div className='articleDetailWrapper'>
           {articles
             .filter((el) => el.id === parseInt(id))
             .map((result) => {
               return (
                 <>
-                  <div className="stateCatArticlesCat">
-                    <Link to="/articlesGrid"> 
-                      <button className="btnCat ">{result.nom_categorie}</button>
+                  <div className='stateCatArticlesCat'>
+                    <Link to='/articlesGrid' className='btnDetail '>
+                      <div>{result.nom_categorie}</div>
                     </Link>
-                    <img src={fleche} alt="fleche" className="gridArrow" />
-                    <Link to="/articlesGrid">
-                      <button className="btnSousCat ">
-                      {result.nom_sous_categorie}
-                      </button>
+                    <img src={fleche} alt='fleche' className='gridArrow' />
+                    <Link to='/articlesGrid' className='btnDetail '>
+                      <div>{result.nom_sous_categorie}</div>
                     </Link>
                   </div>
-                  <h2 className="articleTitreH2">{result.titre} </h2>
-                  <div className="articleDetailIntro">
-                    <p id="articleDetailIntro">{result.intro}</p>
+                  <h2 className='articleTitreH2'>{result.titre} </h2>
+                  <div className='articleDetailIntro'>
+                    <p id='articleDetailIntro'>{result.intro}</p>
                   </div>
-                  <div className="articleDetailImage">
+                  <div className='articleDetailImage'>
                     <img
                       src={result.image}
                       alt={result.tire}
-                      className="articleImage"
+                      className='articleImage'
                     ></img>
                   </div>
-                  <div className="articleDetailFirst">
-                    <div className="boutonTelechargementDoc">
-                      <Link to="/articlesGrid">
-                        <button className="buttonGreen ">Télécharger</button>
+                  <div className='articleDetailFirst'>
+                    <div className='boutonTelechargementDoc'>
+                      <Link to='/articlesGrid'>
+                        <button className='buttonGreen '>Télécharger</button>
                       </Link>
                     </div>
-                    <div className="articleDetailPara">
-                      <p id="articleDetailPara">{result.para1}</p>
+                    <div className='articleDetailPara'>
+                      <p id='articleDetailPara'>{result.para1}</p>
                     </div>
-                    <div className="articleDetailPara">
-                      <p id="articleDetailPara">{result.avantage}</p>
+                    <div className='articleDetailPara'>
+                      <p id='articleDetailPara'>{result.avantage}</p>
                     </div>
-                    <div className="boutonTelechargementDoc">
-                      <Link to="/articlesGrid">
-                        <button className="buttonGreen ">Télécharger</button>
+                    <div className='boutonTelechargementDoc'>
+                      <Link to='/articlesGrid'>
+                        <button className='buttonGreen '>Télécharger</button>
                       </Link>
                     </div>
-
                   </div>
                 </>
-              );
+              )
             })}
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default ArticleDetail;
+export default ArticleDetail
