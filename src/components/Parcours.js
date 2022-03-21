a720ecb747e11d20df771a6fb690a6f075335300
Martin Clara <clara-martin_student2021@wilder.school>
2022-03-21 10:23:45 +0100

    update parcourd

--- src/components/Parcours.js
+++ src/components/Parcours.js
@@ -1,15 +1,26 @@
 import './Styles/Parcours.css'
-import { useState } from 'react'
-// import Snake from "../assets/snake.svg";
-// import arrow from "../assets/icones/next.png"
-import arrow from '../assets/icones/play.png'
+// import { useState } from 'react'
+import { useContext } from 'react'
+import { CategoriesContext } from '../context/CategoriesContext'
+// import arrow from '../assets/icones/play.png'
 
 const Parcours = () => {
+  const { categories } = useContext(CategoriesContext)
+  const colors = ['purple', 'yellow', 'orange', 'brown', 'black']
+
   return (
     <div className='snake-bloc'>
       <div className='snake'>
         {/* <div className="block">efefe</div> */}
-        <button className='parcourBtn orange5 borderRadius'>JEUNESSE</button>
+        {console.log(categories.value)}
+        {categories.map((categorie) => {
+          return (
+            <button className='parcourBtn borderRadius'>
+              {categorie.value}
+            </button>
+          )
+        })}
+        {/* <button className='parcourBtn orange5 borderRadius'>JEUNESSE</button>
         <button className='parcourBtn orange4 borderRadius'>
           PRÉ-INCUBATION
         </button>
@@ -27,31 +38,9 @@ const Parcours = () => {
         </button>
         <div className='cache'></div>
         <img className='snakeArrow' src={arrow} alt='arrow' />
-        {/* <div className="block">azaz</div> */}
+        <div className="block">azaz</div> */}
       </div>
     </div>
-    // <div className="wrapperSnake" style={{ backgroundImage: `url(${Snake})` }}>
-    //   <div className="SnakeLine">
-    //     <button className="parcourBtn orange5 borderRadius">JEUNESSE</button>
-    //     <button className="parcourBtn orange4 borderRadius">
-    //       PRÉ-INCUBATION
-    //     </button>
-    //     <button className="parcourBtn orange3 borderRadius">IDEATION</button>
-    //     <button className="parcourBtn orange2 borderRadius">AMORÇAGE</button>
-    //     <button className="parcourBtn orange1 borderRadius">INCUBATION</button>
-    //   </div>
-    //   <div className="SnakeLine">
-    //     <button className="parcourBtn green5 borderRadius">CRÉATION</button>
-    //     <button className="parcourBtn green4 borderRadius">
-    //       OUTILS DE GESTION
-    //     </button>
-    //     <button className="parcourBtn green3 borderRadius">CONCOURS</button>
-    //     <button className="parcourBtn green2 borderRadius">R&#38;D</button>
-    //     <button className="parcourBtn green borderRadius">
-    //       LEVÉE DE FONDS
-    //     </button>
-    //   </div>
-    // </div>
   )
 }
 export default Parcours
