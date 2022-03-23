c2b9d7af7c367421e87cb0ac2baa29364becc09c
Pinchon Léo <leopinchonspa@gmail.com>
2022-03-23 17:41:53 +0100

    no modif

--- src/context/SousCategoriesContext.js
+++ src/context/SousCategoriesContext.js
@@ -5,32 +5,31 @@ import { ArticleContext } from "./ArticleContext";
 
 export const SousCategoriesContext = createContext();
 const SousCategoriesContextProvider = (props) => {
-
- 
-
-const [filters,setFilters]=useState('')
-const [idCatSousCat,setIdCatSousCat]=useState()  
-const [sousCategories, setSousCategories] = useState([]);
-useEffect (()=> {
-  let filter=[]
-  idCatSousCat && filter.push(`categorie=${idCatSousCat}`)
-  // sousCategories&& filter.push(`id=${sousCategories}`)
-  setFilters(`?${filter.join("&")}`)
-},[idCatSousCat])
-
-const sousCatSet =(id)=>{
-  setIdCatSousCat(id)
-  axios
-  .get(`http://localhost:4242/souscategories/?categorie=${idCatSousCat}`)
-  .then((res)=> setSousCategories(res.data))
-}
+  const [filters, setFilters] = useState("");
+  const [idCatSousCat, setIdCatSousCat] = useState();
+  const [sousCategories, setSousCategories] = useState([]);
   useEffect(() => {
+    let filter = [];
+    idCatSousCat && filter.push(`categorie=${idCatSousCat}`);
+    // sousCategories&& filter.push(`id=${sousCategories}`)
+    setFilters(`?${filter.join("&")}`);
+  }, [idCatSousCat]);
+
+  const sousCatSet = (id) => {
+    setIdCatSousCat(id);
     axios
-      .get(`http://localhost:4242/souscategories${filters}`)
+      .get(`http://localhost:4242/souscategories/?categorie=${idCatSousCat}`)
       .then((res) => setSousCategories(res.data));
-  }, [filters],console.log('amonde',filters));
-
-
+  };
+  useEffect(
+    () => {
+      axios
+        .get(`http://localhost:4242/souscategories${filters}`)
+        .then((res) => setSousCategories(res.data));
+    },
+    [filters],
+    console.log("amonde", filters)
+  );
 
   return (
     <SousCategoriesContext.Provider value={{ sousCategories, sousCatSet }}>
